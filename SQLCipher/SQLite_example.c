ef443106af410a57ec9a23a5654676a1ae016a06
Federico Pereira <lord.basex@gmail.com>
2018-03-25 16:00:28 -0300

    Create SQLite_example.c

---
+++ SQLCipher/SQLite_example.c
@@ -0,0 +1,155 @@
+/* 
+   ******************************************
+   SQLite minimal operation pseudo code
+   ******************************************
+   
+   configure(GLOBAL, param)
+      limit(LOCAL, param)
+      db_config(LOCAL, param)
+   open(DB)
+   exec(COMMAND, param)
+      prepare(COMMAND, param)
+      bind(param, value)
+      step(COMMAND, param)
+      collumn(COMMAND)
+      reset(COMMAND)
+      finalize(COMMAND)
+   close(DB)
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "sqlite3.h"
+
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName){
+   int i;
+   for(i=0; i<argc; i++){
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
+
+int main(int argc, char* argv[])
+{
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int  rc;
+   char *sql;
+   char sqlCommand[400];
+   const char* data = "Callback function called";
+
+   /* Open database */
+   rc = sqlite3_open("test.db", &db);
+   if( rc ){
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      exit(0);
+   }else{
+      fprintf(stdout, "Opened database successfully\n");
+   }
+	
+   /* Encrypt database */
+   rc = sqlite3_key(db, "1q2w3e4r", 8);
+   if( rc ){
+      fprintf(stderr, "Can't encrypt database: %s\n", sqlite3_errmsg(db));
+      exit(0);
+   }else{
+      fprintf(stdout, "Database encrypted successfully\n");
+   }
+
+   /* Create SQL statement */
+   sql = "CREATE TABLE COMPANY("  \
+         "ID INT PRIMARY KEY     NOT NULL," \
+         "NAME           TEXT    NOT NULL," \
+         "AGE            INT     NOT NULL," \
+         "ADDRESS        CHAR(50)," \
+         "SALARY         REAL );";
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   if( rc != SQLITE_OK ){
+   fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   }else{
+      fprintf(stdout, "Table created successfully\n");
+   }
+
+   sprintf (sqlCommand,\
+      "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "\
+      "VALUES (%d,'%s',%d,'%s',%.2lf); "\
+      "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "\
+      "VALUES (%d,'%s',%d,'%s',%.2lf); "\
+      "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "\
+      "VALUES (%d,'%s',%d,'%s',%.2lf); "\
+      "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "\
+      "VALUES (%d,'%s',%d,'%s',%.2lf); "\
+      ,1, "Paul", 32, "California", 20000.00
+      ,2, "Allen", 25, "Texas", 15000.00
+      ,3, "Teddy", 23, "Norway", 20000.00
+      ,4, "Mark", 25, "Rich-Mond", 65000.00);
+
+   /* Create SQL statement */
+   /*
+   sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
+         "VALUES (1, 'Paul', 32, 'California', 20000.00 ); " \
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
+         "VALUES (2, 'Allen', 25, 'Texas', 15000.00 ); "     \
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
+         "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" \
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
+         "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+   */
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sqlCommand, callback, 0, &zErrMsg);
+   if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   }else{
+      fprintf(stdout, "Records created successfully\n");
+   }
+
+   /* Create SQL statement */
+   sql = "SELECT * from COMPANY";
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+   if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   }else{
+      fprintf(stdout, "Operation done successfully\n");
+   }
+
+   /* Create merged SQL statement */
+   sql = "UPDATE COMPANY set SALARY = 25000.00 where ID=1; " \
+         "SELECT * from COMPANY";
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+   if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   }else{
+      fprintf(stdout, "Operation done successfully\n");
+   }
+
+      /* Create merged SQL statement */
+   sql = "DELETE from COMPANY where ID=2; " \
+         "SELECT * from COMPANY";
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+   if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   }else{
+      fprintf(stdout, "Operation done successfully\n");
+   }
+
+   sqlite3_close(db);
+   return 0;
+}
